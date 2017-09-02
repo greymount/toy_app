77943c532583c5e6f3003ddc26b01077878f063d
Justinus Raymond <raymondjustinus@rocketmail.com>
2017-09-02 08:58:47 +0000

    Update all

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
 
+  resources :microposts
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -56,5 +57,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-root 'application#hello'
+root 'microposts#index'
 end
